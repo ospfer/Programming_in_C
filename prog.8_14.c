5534f546ca4865ea02a5b89c1e9679ed903c879d
ospfer <kevin.venanzi@gmail.com>
2017-08-07 08:10:39 -0500

    Cleanup

--- prog.8_14.c
+++ prog.8_14.c
@@ -1,4 +1,4 @@
-//Program to convert a positive integer to another base
+//Program to convert a positive integer to another base 
 
 #include <stdio.h>
 
@@ -34,7 +34,7 @@ void convertNumber (void)
 
 void displayConvertedNumber(void)
 {
-	const char baseDigits[16] = 
+	const char baseDigits[16] =
 	{ '0', '1', '2', '3', '4', '5', '6', '7',
 		'8', '9', 'A', 'B', 'C', 'D', 'E', 'F'	};
 	int nextDigit;
