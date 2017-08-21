01290351e4dd24157e85131202fed6b7e3f12ad5
ospfer <kevin.venanzi@gmail.com>
2017-08-21 15:29:47 -0500

    Chapter 9 exercise
    Calculate elapsed days between two dates.

---
+++ ex.9_2.c
@@ -0,0 +1,44 @@
+//Program to calculate number of days between two dates
+
+#include <stdio.h>
+
+struct date
+{
+	int month;
+	int day;
+	int year;
+};
+
+//Funtction that accepts date structure and return value of N
+int elapsed (struct date d)
+{
+	int n, f, g;
+	if (d.month <= 2)
+	{
+		f = d.year - 1;
+		g = d.month + 13;
+	}
+	else
+	{
+		f = d.year;
+		g = d.month + 1;
+	}
+
+	n = 1461 * f / 4 + 153 * g / 5 + d.day;
+}
+
+int main (void)
+{
+	int elapsedDays;
+	struct date date1, date2;
+
+	printf("Enter two dates to calculate the elapsed days between them.\n");
+	printf("Date 1 (mm dd yyyy): ");
+	scanf("%i%i%i", &date1.month, &date1.day, &date1.year);
+	printf("Date 2 (mm dd yyyy): ");
+	scanf("%i%i%i", &date2.month, &date2.day, &date2.year);
+
+	elapsedDays = elapsed(date2) - elapsed(date1);
+
+	printf("\nThere are %i days between %i/%i/%i and %i/%i/%i.\n", elapsedDays, date1.month, date1.day, date1.year, date2.month, date2.day, date2.year);
+}
